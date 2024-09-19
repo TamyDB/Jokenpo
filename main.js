ef65e2e46cfd914ebe3d39c12b8568cd437978c0
Rafael Tamy <rafaeltamydb@gmail.com>
2024-09-19 17:53:00 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const result = document.getElementById('win')
+const rock = document.getElementById('rock')
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+const options = ['rock', 'paper', 'scissors']
+
+let playerOption
+
+rock.addEventListener('click', () => {
+    playerOption = options[0]
+    gameLoop()
+})
+
+paper.addEventListener('click', () => {
+    playerOption = options[1]
+    gameLoop()
+})
+
+scissors.addEventListener('click', () => {
+    playerOption = options[2]
+    gameLoop()
+})
+
+function gameLoop() {
+    const botOption = options[Math.floor(Math.random() * options.length)]
+
+    winner(playerOption, botOption)
+}
+
+function winner(player, bot) {
+    if (player === bot) {
+        result.innerText = 'Empate'
+    } else if (
+        (player === 'rock' && bot === 'scissors') ||
+        (player === 'paper' && bot === 'rock') ||
+        (player === 'scissors' && bot === 'paper')
+    ) {
+        result.innerText = 'Jogador venceu'
+    } else {
+        result.innerText = 'Bot venceu'
+    }
+}
